7f5d2370ca69ab84d9420d182b59a671c732635d
tuankthai <ttlive4jc@gmail.com>
2023-04-19 00:34:52 -0700

    enhance personal web site

--- javascript.js
+++ javascript.js
@@ -1,1 +1,23 @@
 console.log("Hello World")
+
+const form = document.querySelector("#form")
+const submitButton = document.querySelector("#submit")
+const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
+
+form.addEventListener('submit', e => {
+    // console.log("you click submit button")
+    submitButton.disabled = true
+    e.preventDefault()
+    let requestBody = new FormData(form)
+    fetch(scriptURL, { method: 'POST', body: requestBody })
+        .then(response => {
+            alert('Success!', response)
+            submitButton.disabled = false
+        })
+        .catch(error => {
+            alert('Error!', error.message)
+            submitButton.disabled = false
+
+        }
+        )
+})
